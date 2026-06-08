9724921b65fc4c80b4be0957578d1587eeb48c23
ycgao <yc_x@outlook.com>
2026-06-08 16:05:32 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,7 @@ export default class Main extends Plugin {
     async onload() {
         console.log('Loading plugin: ' + this.manifest.name);
 
-        this.registerMarkdownCodeBlockProcessor('runjs', async (source, el, ctx) => {
-            eval(source)
-        });
-        this.registerMarkdownCodeBlockProcessor('echart', async (source, el, ctx) => {
+        this.registerMarkdownCodeBlockProcessor('echart', async (source) => {
             eval(`${source}
                 el.style.width = '100%'
                 el.style.aspectRatio = '16 / 9'
